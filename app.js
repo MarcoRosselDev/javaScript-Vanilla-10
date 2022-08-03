57971f6401bfc7d1e74da6cb4df2d99312960d41
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-02 23:16:38 -0400

    detecting scrolling succesfull

--- app.js
+++ app.js
@@ -29,7 +29,11 @@ window.addEventListener('scroll', function(){
   const scrollHeight = window.pageYOffset;
   const navHeight = navbar.getBoundingClientRect().height;
 
-  
+  if(scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
+  }
 })
 // ********** smooth scroll ************
 // select links
